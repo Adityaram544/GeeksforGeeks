42be2293c7cc206b2a2255a7f4705b4e33611b25
Narala.Aditya ram <150006848+Adityaram544@users.noreply.github.com>
2026-03-22 11:03:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+        // Code here
+        String str[]=s.split("\\.");
+        StringBuilder sb=new StringBuilder();
+        for(int i=str.length-1;i>=0;i--){
+            if(str[i].length()>0){
+                if(sb.length()>0) sb.append(".");
+                sb.append(str[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
