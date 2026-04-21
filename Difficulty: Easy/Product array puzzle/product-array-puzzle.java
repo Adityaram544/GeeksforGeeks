c4dbdd6eb615e7e9fdfd40c4b16a404459cd8e96
Narala.Aditya ram <150006848+Adityaram544@users.noreply.github.com>
2026-04-21 22:53:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int[] productExceptSelf(int arr[]) {
+        // code here
+        int n = arr.length;
+        int[] res = new int[n];
+        // Left product
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * arr[i - 1];
+        }
+        // Right product
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = res[i] * right;
+            right = right * arr[i];
+        }
+        return res;
+    }
+}
