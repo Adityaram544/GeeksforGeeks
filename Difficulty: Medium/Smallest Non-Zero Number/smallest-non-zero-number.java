fa154082e428087cbdd023809598b7649f8417bd
Narala.Aditya ram <150006848+Adityaram544@users.noreply.github.com>
2026-07-14 09:51:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Non-Zero Number/smallest-non-zero-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int find(int[] arr) {
+        // code here
+        int x=0;
+        for(int i=arr.length-1;i>=0;i--){
+            x=(x+arr[i]+1)/2;
+        }
+        return Math.max(1,x);
+    }
+}
